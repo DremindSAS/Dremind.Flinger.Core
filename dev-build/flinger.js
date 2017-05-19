eb3f3fec20d8279c2a7dcba5ca50c3afc43fefb9
Cami Rodriguez <camiepisode@outlook.com>
2017-05-19 03:23:01 -0500

    Updated uri

--- dev-build/flinger.js
+++ dev-build/flinger.js
@@ -1,4 +1,4 @@
-/*! crawlersite.kernel - v0.0.1 - 2017-05-18 */
+/*! crawlersite.kernel - v0.0.1 - 2017-05-19 */
 var Cross = (function () {
     var _timeStamp;
     var _serverUri;
@@ -436,7 +436,7 @@ var SocketHub = (function () {
         var script = document.createElement('script');
         script.type = 'text/javascript';
         script.onload = socketLibrary_loaded;
-        script.src = 'http://localhost:3501/build/assets/socket.io-1.4.5.js';
+        script.src = 'http://localhost:3500/socket.io.js';
         head.appendChild(script);
     }
 
